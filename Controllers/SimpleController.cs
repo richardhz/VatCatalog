2a6c187125993892f0856b8d328acca2fcf6a094
Richard Hernandez <richardhz@users.noreply.github.com>
2021-09-29 15:46:54 +0100

    Add project files.

---
+++ Controllers/SimpleController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VatCatalog.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleController : ControllerBase
+    {
+        private readonly ILogger<SimpleController> _logger;
+
+        // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
+        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
+
+        public SimpleController(ILogger<SimpleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+
+            var rng = new Random();
+            return Ok("Hello");
+        }
+    }
+}
